742bc1c0a396913872dbbabbf4f459cf3e5c3ee2
ChihPing.Weng <chihping.weng@ChihPingWengs-MacBook-Pro.local>
2020-02-11 17:26:20 +0800

    Add swipe views with tabs

---
+++ appier-mediation-sample/src/main/java/com/appier/sampleapp/AppierNativeManualIntegrationTabFragment1.java
@@ -0,0 +1,17 @@
+package com.appier.sampleapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AppierNativeManualIntegrationTabFragment1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_appier_native_manual_integration_tab_fragment_1, container, false);
+    }
+}
