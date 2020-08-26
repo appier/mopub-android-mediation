a5c6f187293b644d2a8b1f38dbb561131152578e
Justin Kuo <justin.kuo@appier.com>
2020-08-26 19:01:42 +0800

    feat: `AppierPredictHandler` implements `AppierPredictor.EventListener` to prefetch image after predict

--- appier-mediation/src/main/java/com/mopub/mobileads/AppierPredictHandler.java
+++ appier-mediation/src/main/java/com/mopub/mobileads/AppierPredictHandler.java
@@ -1,9 +1,25 @@
 package com.mopub.mobileads;
 
-import com.appier.ads.AppierPredictCache;
-import com.mopub.nativeads.RequestParameters;
+import android.content.Context;
 
-public class AppierPredictHandler {
+import com.appier.ads.Appier;
+import com.appier.ads.AppierPredictor;
+import com.appier.ads.AppierError;
+import com.appier.ads.AppierPredictCache;
+import com.mopub.nativeads.NativeErrorCode;
+import com.mopub.nativeads.NativeImageHelper;
+import com.mopub.nativeads.RequestParameters;
+import static com.mopub.nativeads.NativeImageHelper.preCacheImages;
+
+import java.util.List;
+
+public class AppierPredictHandler implements AppierPredictor.EventListener {
+    private Context mContext;
+
+    public AppierPredictHandler(Context context) {
+        mContext = context;
+    }
+
     private static String getKeywordTargeting(String zoneId) {
         if (AppierPredictCache.getInstance().getPredictResult(zoneId)) {
             return "appier_predict:1";
@@ -27,5 +43,31 @@ public class AppierPredictHandler {
 
     public static void setKeywordTargeting(String zoneId, MoPubInterstitial moPubInterstitial) {
         moPubInterstitial.setKeywords(AppierPredictHandler.getKeywordTargeting(zoneId));
+    }
+
+    @Override
+    public void onPredictBidAndGetPrefetchList(final String adUnitId, List<String> prefetchList) {
+        Appier.log("[Appier Mediation]", "[Predict Mode]", "successfully predict ad ", adUnitId, ": Bid");
+        preCacheImages(mContext, prefetchList, new NativeImageHelper.ImageListener() {
+            @Override
+            public void onImagesCached() {
+                Appier.log("[Appier Mediation]", "[Predict Mode]", "successfully cache images for ad: ", adUnitId);
+            }
+
+            @Override
+            public void onImagesFailedToCache(NativeErrorCode errorCode) {
+                Appier.log("[Appier Mediation]", "[Predict Mode]", "failed to cache images for ad: ", adUnitId);
+            }
+        });
+    }
+
+    @Override
+    public void onPredictNoBid(String adUnitId) {
+        Appier.log("[Appier Mediation]", "[Predict Mode]", "successfully predict ad ", adUnitId, ": NoBid");
+    }
+
+    @Override
+    public void onPredictFailed(String adUnitId, AppierError error) {
+        Appier.log("[Appier Mediation]", "[Predict Mode]", "predict ad ", adUnitId, " failed: ", error);
     }
 }
